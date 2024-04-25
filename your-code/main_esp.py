1afa2b110d52d1dfb155482bfcb5e284bcb85bbb
TheMineWay <joelcamposoliva9000@gmail.com>
2024-04-25 19:15:44 +0200

    Transpose

--- your-code/main_esp.py
+++ your-code/main_esp.py
@@ -1,40 +1,45 @@
 #1. Importa el paquete NUMPY bajo el nombre np.
 
-from numpy import version, np
+import numpy as np
 
 #2. Imprime la versión de NUMPY y la configuración.
 
-print(version.version)
+print(np.version.version)
 
 #3. Genera un array tridimensional de 2x3x5 con valores aleatorios. Asigna el array a la variable "a"
 # Desafío: hay al menos tres maneras fáciles que usan numpy para generar arrays aleatorios. ¿Cuántas formas puedes encontrar?
 
-#[tu código aquí]
+a = np.random.rand(2, 3, 5)
 
 #4. Imprime a.
 
-#[tu código aquí]
+print(a)
+
 #5. Crea un array tridimensional de 5x2x3 con todos los valores igual a 1.
 #Asigna el array a la variable "b"
 
-#[tu código aquí]
+b = np.ones((5,2,3))
 
 #6. Imprime b.
 
-#[tu código aquí]
+print(b)
 
 #7. ¿Tienen a y b el mismo tamaño? ¿Cómo lo demuestras en código Python?
 
-#[tu código aquí]
+equal = a.shape == b.shape
+print(equal)
 
 #8. ¿Es posible sumar a y b? ¿Por qué sí o por qué no?
 
-#[tu código aquí]
-
+try:
+    sum = np.sum(a,b)
+except:
+    print("Error al sumar")
+# No es posible sumar ya que las listas no son iguales
 
 #9. Transpone b para que tenga la misma estructura que a (es decir, se convierta en un array de 2x3x5). Asigna el array transpuesto a la variable "c".
 
-#[tu código aquí]
+c = np.transpose(b, (1, 2, 0))
 
 #10. Intenta sumar a y c. Ahora debería funcionar. Asigna la suma a la variable "d". Pero, ¿por qué funciona ahora?
 
